abde70f0ba5d240335e61a74ba1b8082b97d60b7
Stephen Smith <ssmith_138823@outlook.com>
2024-04-02 21:02:47 -0400

    Pushing to GitHub

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?"); //if you want the user to know what you want as input then you must place this before the Console.ReadLine()
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age + " years old!");//if incorrect data type is used then an exception is created.  Will come back to exceptions later  
+        }
+    }
+}
